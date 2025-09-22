51e28472001870dd042638a707702dd18a2e5f8a
maria eduarda <maria.e.ferreira77@aluno.senai.br>
2025-09-22 14:01:42 -0300

    finaliza os dados

---
+++ src/models/dados.js
@@ -0,0 +1,106 @@
+export const conteudos = [
+
+    {
+        id: 1,
+        título: "A Queda de Icarus",
+        categoria: "Quadrinhos",
+        reviewer: "Pedro Mente Aberta",
+        nota: 8.5,
+        data_plataforma: "20/09/2025",
+        visualizações: 12.450,
+        comentários: 321
+
+    },
+    {
+        id: 2,
+        título: "Cyber-Samurai: Ascensão",
+        categoria: "Animes",
+        reviewer: "Ana Otaku Real",
+        nota: 9.2,
+        data_plataforma: "18/09/2025",
+        visualizações: 23100,
+        comentários: 789
+    },
+    {
+        id: 3,
+        título: "O Enigma da Lua Vermelha",
+        categoria: "Filmes",
+        reviewer: "Lucas Cinéfilo Nerd",
+        nota: 7.8,
+        data_plataforma: "15/09/2025",
+        visualizações: 8900,
+        comentários: 154
+    },
+    {
+        id: 4,
+        título: "Aventura na Galáxia Proibida",
+        categoria: "Jogos",
+        reviewer: "Sofia Gamergirl",
+        nota: 9.5,
+        data_plataforma: "12/09/2025",
+        visualizações: 45300,
+        comentários: 1250
+    },
+    {
+        id: 5,
+        título: "A Crônica de Gelo e Vapor",
+        categoria: "Livros",
+        reviewer: "Daniel Literato Geek",
+        nota: 8.1,
+        data_plataforma: "10/09/2025",
+        visualizações: 5600,
+        comentários: 98
+    },
+    {
+        id: 6,
+        título: "O Legado do Rei Demônio",
+        categoria: "Animes",
+        reviewer: "Ana Otaku Real",
+        nota: 7.9,
+        data_plataforma: "05/09/2025",
+        visualizações: 15700,
+        comentários: 412
+    },
+    {
+        id: 7,
+        título: "O Último Guardião",
+        categoria: "Filmes",
+        reviewer: "Lucas Cinéfilo Nerd",
+        nota: 9.0,
+        data_plataforma: "01/09/2025",
+        visualizações: 31800,
+        comentários: 950
+    },
+    {
+        id: 8,
+        título: "O Labirinto de Esmeralda",
+        categoria: "Jogos",
+        reviewer: "Sofia Gamergirl",
+        nota: 8.8,
+        data_plataforma: "28/08/2025",
+        visualizações: 21500,
+        comentários: 630
+    },
+    {
+        id: 9,
+        título: "A Dinastia do Dragão",
+        categoria: "Quadrinhos",
+        reviewer: "Pedro Mente Aberta",
+        nota: 9.4,
+        data_plataforma: "25/08/2025",
+        visualizações: 18900,
+        comentários: 520
+    },
+    {
+        id: 10,
+        título: "A Saga dos Nove Reinos",
+        categoria: "Livros",
+        reviewer: "Daniel Literato Geek",
+        nota: 7.5,
+        data_plataforma: "20/08/2025",
+        visualizações: 7200,
+        comentários: 115
+    },
+];
+
+export default conteudos;
